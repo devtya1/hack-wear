1f34fff23ce324a529a9d98f5bb2786ff7e4d29e
devtya1 <98102495+devtya1@users.noreply.github.com>
2024-10-20 15:14:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var code = `
 <?php
 echo "danbulant.eu presents...";
-$someVar[0] = "Hacker Typer..";`;
+$someVar[0] = "Hacker Typer..";
+echo "Loading system files...";
+echo "Connecting to the server...";
+?>`;
 
-var OLDcode = `#include <linux/delay.h>
+var OLDcode = `
+#include <linux/delay.h>
 #include <linux/init.h>
 #include <linux/irqdomain.h>
 #include <linux/pci.h>
@@ -20,88 +24,20 @@ var OLDcode = `#include <linux/delay.h>
  * https://www.pcisig.com/members/downloads/pcifw_r3_1_13Dec10.pdf
  */
 const u8 pci_acpi_dsm_uuid[] = {
-	0xd0, 0x37, 0xc9, 0xe5, 0x53, 0x35, 0x7a, 0x4d,
-	0x91, 0x17, 0xea, 0x4d, 0x19, 0xc3, 0x43, 0x4d
+    0xd0, 0x37, 0xc9, 0xe5, 0x53, 0x35, 0x7a, 0x4d,
+    0x91, 0x17, 0xea, 0x4d, 0x19, 0xc3, 0x43, 0x4d
 };
 phys_addr_t acpi_pci_root_get_mcfg_addr(acpi_handle handle)
 {
-	acpi_status status = AE_NOT_EXIST;
-	unsigned long long mcfg_addr;
-	if (handle)
-		status = acpi_evaluate_integer(handle, METHOD_NAME__CBA,
-					       NULL, &mcfg_addr);
-	if (ACPI_FAILURE(status))
-		return 0;
-	return (phys_addr_t)mcfg_addr;
-}
-static acpi_status decode_type0_hpx_record(union acpi_object *record,
-					   struct hotplug_params *hpx)
-{
-	int i;
-	union acpi_object *fields = record->package.elements;
-	u32 revision = fields[1].integer.value;
-	switch (revision) {
-	case 1:
-		if (record->package.count != 6)
-			return AE_ERROR;
-		for (i = 2; i < 6; i++)
-			if (fields[i].type != ACPI_TYPE_INTEGER)
-				return AE_ERROR;
-		hpx->t0 = &hpx->type0_data;
-		hpx->t0->revision        = revision;
-		hpx->t0->cache_line_size = fields[2].integer.value;
-		hpx->t0->latency_timer   = fields[3].integer.value;
-		hpx->t0->enable_serr     = fields[4].integer.value;
-		hpx->t0->enable_perr     = fields[5].integer.value;
-		break;
-	default:
-		printk(KERN_WARNING
-		       "%s: Type 0 Revision %d record not supported\n",
-		       __func__, revision);
-		return AE_ERROR;
-	}
-	return AE_OK;
-}
-static acpi_status decode_type1_hpx_record(union acpi_object *record,
-					   struct hotplug_params *hpx)
-{
-	int i;
-	union acpi_object *fields = record->package.elements;
-	u32 revision = fields[1].integer.value;
-	switch (revision) {
-	case 1:
-		if (record->package.count != 5)
-			return AE_ERROR;
-		for (i = 2; i < 5; i++)
-			if (fields[i].type != ACPI_TYPE_INTEGER)
-				return AE_ERROR;
-		hpx->t1 = &hpx->type1_data;
-		hpx->t1->revision      = revision;
-		hpx->t1->max_mem_read  = fields[2].integer.value;
-		hpx->t1->avg_max_split = fields[3].integer.value;
-		hpx->t1->tot_max_split = fields[4].integer.value;
-		break;
-	default:
-		printk(KERN_WARNING
-		       "%s: Type 1 Revision %d record not supported\n",
-		       __func__, revision);
-		return AE_ERROR;
-	}
-	return AE_OK;
-}
-static acpi_status decode_type2_hpx_record(union acpi_object *record,
-					   struct hotplug_params *hpx)
-{
-	int i;
-	union acpi_object *fields = record->package.elements;
-	u32 revision = fields[1].integer.value;
-	switch (revision) {
-	case 1:
-		if (record->package.count != 18)
-			return AE_ERROR;
-		for (i = 2; i < 18; i++)
-			if (fields[i].type != ACPI_TYPE_INTEGER)
-				return AE_ERROR;`;
+    acpi_status status = AE_NOT_EXIST;
+    unsigned long long mcfg_addr;
+    if (handle)
+        status = acpi_evaluate_integer(handle, METHOD_NAME__CBA,
+                                       NULL, &mcfg_addr);
+    if (ACPI_FAILURE(status))
+        return 0;
+    return (phys_addr_t)mcfg_addr;
+}`;
 
 // Function to display code line by line
 function displayCode(code) {
@@ -112,22 +48,44 @@ function displayCode(code) {
     // Function to append a line to the terminal
     function appendLine() {
         if (currentLine < lines.length) {
-            terminal.innerText += lines[currentLine] + '\n'; // Add the line to terminal
+            // Check for special 'confirm' action
+            if (lines[currentLine].includes("<?php echo 'Send virus?' ?>")) {
+                // If confirm action, pause and ask the user
+                if (confirm("Send virus?")) {
+                    terminal.innerText += "[CONFIRMED] Virus sent...\n";
+                } else {
+                    terminal.innerText += "[CANCELED] Action aborted.\n";
+                }
+            } else {
+                // Add the normal line to terminal
+                terminal.innerText += lines[currentLine] + '\n';
+            }
             currentLine++; // Move to the next line
-            setTimeout(appendLine, 100); // Call appendLine again after 100 ms
+            setTimeout(appendLine, 200); // Call appendLine again after 200ms
         }
     }
 
     appendLine(); // Start appending lines
 }
 
+// Play sound when the button is clicked
+function playSound() {
+    var audio = new Audio('sound.mp4');
+    audio.play();
+}
+
 document.getElementById("hack").addEventListener("click", function() {
     // Hide the button
     this.style.display = "none";
 
+    // Play the sound
+    playSound();
+
     // Display the code line by line
     displayCode(code);
-    displayCode(OLDcode); // If you want to display OLDcode as well
+    setTimeout(function() {
+        displayCode(OLDcode); // Display OLDcode after code is done
+    }, code.split('\n').length * 200); // Adjust the timing based on code length
 });
 
 // Scroll terminal to the bottom every 100ms
